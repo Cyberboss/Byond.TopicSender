3d88e462f8c3512a9a6d0b2ccf8fb6f6d31a4867
Jordan Brown <Cyberboss@users.noreply.github.com>
2020-05-07 10:22:15 -0400

    Apply C#8 features

--- Byond.TopicSender/ByondTopicSender.cs
+++ Byond.TopicSender/ByondTopicSender.cs
@@ -66,92 +66,90 @@ namespace Byond.TopicSender
 			packet[2] = lengthBytes[lilEndy ? 1 : 0];
 			packet[3] = lengthBytes[lilEndy ? 0 : 1];
 
-			using (var topicSender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
+			using var topicSender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
 			{
 				SendTimeout = SendTimeout,
 				ReceiveTimeout = ReceiveTimeout
-			})
+			};
+			//connect
+			var connectTaskCompletionSource = new TaskCompletionSource<object?>();
+			topicSender.BeginConnect(endPoint, new AsyncCallback((asyncResult) =>
 			{
-				//connect
-				var connectTaskCompletionSource = new TaskCompletionSource<object?>();
-				topicSender.BeginConnect(endPoint, new AsyncCallback((asyncResult) =>
+				try
 				{
-					try
-					{
-						topicSender.EndConnect(asyncResult);
-						connectTaskCompletionSource.TrySetResult(null);
-					}
-					catch (Exception e)
-					{
-						connectTaskCompletionSource.TrySetException(e);
-					}
-				}), null);
-				using (cancellationToken.Register(() => connectTaskCompletionSource.TrySetCanceled()))
-					await connectTaskCompletionSource.Task.ConfigureAwait(false);
-				cancellationToken.ThrowIfCancellationRequested();
-
-				//send
-				for (var offset = 0; offset < packet.Length;)
-				{
-					var sendTaskCompletionSource = new TaskCompletionSource<int>();
-					topicSender.BeginSend(packet, offset, packet.Length - offset, SocketFlags.None, new AsyncCallback((asyncResult) =>
-					{
-						try
-						{
-							sendTaskCompletionSource.TrySetResult(topicSender.EndSend(asyncResult));
-						}
-						catch (Exception e)
-						{
-							sendTaskCompletionSource.TrySetException(e);
-						}
-					}), null);
-					using (cancellationToken.Register(() => sendTaskCompletionSource.TrySetCanceled()))
-						offset += await sendTaskCompletionSource.Task.ConfigureAwait(false);
-					cancellationToken.ThrowIfCancellationRequested();
+					topicSender.EndConnect(asyncResult);
+					connectTaskCompletionSource.TrySetResult(null);
 				}
+				catch (Exception e)
+				{
+					connectTaskCompletionSource.TrySetException(e);
+				}
+			}), null);
+			using (cancellationToken.Register(() => connectTaskCompletionSource.TrySetCanceled()))
+				await connectTaskCompletionSource.Task.ConfigureAwait(false);
+			cancellationToken.ThrowIfCancellationRequested();
 
-				//receive
-				var recieveTaskCompletionSource = new TaskCompletionSource<int>();
-				var returnedData = new byte[UInt16.MaxValue];
-				topicSender.BeginReceive(returnedData, 0, returnedData.Length, SocketFlags.None, new AsyncCallback((asyncResult) =>
+			//send
+			for (var offset = 0; offset < packet.Length;)
+			{
+				var sendTaskCompletionSource = new TaskCompletionSource<int>();
+				topicSender.BeginSend(packet, offset, packet.Length - offset, SocketFlags.None, new AsyncCallback((asyncResult) =>
 				{
 					try
 					{
-						recieveTaskCompletionSource.TrySetResult(topicSender.EndReceive(asyncResult));
+						sendTaskCompletionSource.TrySetResult(topicSender.EndSend(asyncResult));
 					}
 					catch (Exception e)
 					{
-						recieveTaskCompletionSource.TrySetException(e);
+						sendTaskCompletionSource.TrySetException(e);
 					}
 				}), null);
-				using (cancellationToken.Register(() => recieveTaskCompletionSource.TrySetCanceled()))
-					await recieveTaskCompletionSource.Task.ConfigureAwait(false);
+				using (cancellationToken.Register(() => sendTaskCompletionSource.TrySetCanceled()))
+					offset += await sendTaskCompletionSource.Task.ConfigureAwait(false);
 				cancellationToken.ThrowIfCancellationRequested();
-
-				//we need to properly disconnect the socket, otherwise Byond can be an asshole about future sends
-				var disconnectTaskCompletionSource = new TaskCompletionSource<object?>();
-				topicSender.BeginDisconnect(false, new AsyncCallback((asyncResult) =>
-				{
-					try
-					{
-						topicSender.EndDisconnect(asyncResult);
-						disconnectTaskCompletionSource.TrySetResult(null);
-					}
-					catch (Exception e)
-					{
-						disconnectTaskCompletionSource.TrySetException(e);
-					}
-				}), null);
-				using (cancellationToken.Register(() => disconnectTaskCompletionSource.TrySetCanceled()))
-					await disconnectTaskCompletionSource.Task.ConfigureAwait(false);
-				cancellationToken.ThrowIfCancellationRequested();
-
-				//parse
-				var raw_string = Encoding.ASCII.GetString(returnedData).TrimEnd(new char[] { (char)0 }).Trim();
-				if (raw_string.Length > 6)
-					return raw_string.Substring(5, raw_string.Length - 5);
-				return null;
 			}
+
+			//receive
+			var recieveTaskCompletionSource = new TaskCompletionSource<int>();
+			var returnedData = new byte[UInt16.MaxValue];
+			topicSender.BeginReceive(returnedData, 0, returnedData.Length, SocketFlags.None, new AsyncCallback((asyncResult) =>
+			{
+				try
+				{
+					recieveTaskCompletionSource.TrySetResult(topicSender.EndReceive(asyncResult));
+				}
+				catch (Exception e)
+				{
+					recieveTaskCompletionSource.TrySetException(e);
+				}
+			}), null);
+			using (cancellationToken.Register(() => recieveTaskCompletionSource.TrySetCanceled()))
+				await recieveTaskCompletionSource.Task.ConfigureAwait(false);
+			cancellationToken.ThrowIfCancellationRequested();
+
+			//we need to properly disconnect the socket, otherwise Byond can be an asshole about future sends
+			var disconnectTaskCompletionSource = new TaskCompletionSource<object?>();
+			topicSender.BeginDisconnect(false, new AsyncCallback((asyncResult) =>
+			{
+				try
+				{
+					topicSender.EndDisconnect(asyncResult);
+					disconnectTaskCompletionSource.TrySetResult(null);
+				}
+				catch (Exception e)
+				{
+					disconnectTaskCompletionSource.TrySetException(e);
+				}
+			}), null);
+			using (cancellationToken.Register(() => disconnectTaskCompletionSource.TrySetCanceled()))
+				await disconnectTaskCompletionSource.Task.ConfigureAwait(false);
+			cancellationToken.ThrowIfCancellationRequested();
+
+			//parse
+			var raw_string = Encoding.ASCII.GetString(returnedData).TrimEnd(new char[] { (char)0 }).Trim();
+			if (raw_string.Length > 6)
+				return raw_string[5..];
+			return null;
 		}
 
 		/// <inheritdoc />
